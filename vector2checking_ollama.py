904dc04f0d23a7966302bd1c5c5077c4033c237d
Ayush Bhardwaj <116327330+Ayush7970@users.noreply.github.com>
2024-08-11 03:54:28 -0500

    Update vector2checking_ollama.py
    commit 2 
    changes in vector2checking_ollama

--- vector2checking_ollama.py
+++ vector2checking_ollama.py
@@ -26,7 +26,7 @@ documents = GoogleDriveReader(
 # Set up Ollama
 ollama_embedding = OllamaEmbedding(
     model_name="llama3",
-    base_url="http://localhost:11434",
+    base_url=" enter your base url",
     ollama_additional_kwargs={
         "mirostat": 0,
         "num_ctx": 4096,
